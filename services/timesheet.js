21369ccf632e0ba9a39ab9000a6724f865de6c05
Jeet Singh <taranjeet0007@gmail.com>
2023-07-31 12:40:06 +0700

    fix api create

--- services/timesheet.js
+++ services/timesheet.js
@@ -25,31 +25,35 @@ const getCsrfToken = async (date) => {
 };
 
 const createNewTask = async (config, cookies) => {
-    instance.defaults.headers.common['Cookie'] = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-    const formData = new URLSearchParams();
-    const { date, manhours, module, task, subTask, crNo, remark, project, chargeCode, chargeable } =
-        config || {};
-    const { csrfToken, userId } = await getCsrfToken(date);
-    formData.append('authenticity_token', csrfToken);
-    formData.append('commit', 'Create Task');
-    formData.append('task[card_id]', crNo ? `${env.jiraUrl}/browse/${crNo}` : '');
-    formData.append('task[charge_code]', chargeCode || '');
-    formData.append('task[chargeable]', chargeable || 0);
-    formData.append('task[date]', date || moment().format('YYYY-MM-DD'));
-    formData.append('task[hours]', manhours);
-    formData.append('task[module]', module || '');
-    formData.append('task[name]', task || '');
-    formData.append('task[project_id]', project || '');
-    formData.append('task[remark]', remark || '');
-    formData.append('task[requirement_id]', crNo || '');
-    formData.append('task[sub_task]', subTask || '');
-    formData.append('task[user_id]', userId || '');
-    const response = await instance.post('/tasks', formData);
-    // check response status
-    if (response.status !== 200) {
-        throw new Error(`Failed to create task ${config}`);
-    }
-    return response;
+  instance.defaults.headers.common["Cookie"] = cookies
+    .map((cookie) => `${cookie.name}=${cookie.value}`)
+    .join("; ");
+  const formData = new URLSearchParams();
+  const { date, manhours, module, task, subTask, crNo, remark, project } =
+    config || {};
+  const { csrfToken, userId } = await getCsrfToken(date);
+  console.log(config);
+  formData.append("authenticity_token", csrfToken);
+  formData.append("commit", "Create Task");
+  formData.append("task[card_id]", crNo ? `${env.jiraUrl}/browse/${crNo}` : "");
+  formData.append("task[charge_code]", "");
+  formData.append("task[chargeable]", 0);
+  formData.append("task[leave_type_id]", 1);
+  formData.append("task[date]", date);
+  formData.append("task[hours]", manhours);
+  formData.append("task[module]", module || "");
+  formData.append("task[name]", task || "");
+  formData.append("task[project_id]", project || "");
+  formData.append("task[remark]", remark || "");
+  formData.append("task[requirement_id]", crNo);
+  formData.append("task[sub_task]", subTask || "");
+  formData.append("task[user_id]", userId || "");
+  const response = await instance.post("/tasks", formData).catch((err) => {
+    console.warn(err);
+  });
+  if (response?.status !== 200) {
+    throw new Error(`Failed to create task ${JSON.stringify(config)}`);
+  }
 };
 
 module.exports = {
